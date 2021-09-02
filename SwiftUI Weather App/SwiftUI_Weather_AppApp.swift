8ca6c42a9b9fa9d4ac8b92a1a4c2a4d0b45360e5
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-09-02 07:13:02 +0200

    Initial Commit

---
+++ SwiftUI Weather App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI Weather App
+//
+//  Created by Lukasz Dziwosz on 02/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
